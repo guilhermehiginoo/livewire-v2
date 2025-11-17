a63ded535d0752831fa921bb69cb6ed94534f789
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-11-17 08:52:19 -0300

    Update Livewire count component to use wire:model.live for real-time updates

--- resources/views/livewire/count.blade.php
+++ resources/views/livewire/count.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h1>Livewire count component</h1>
     <div>
-        <x-text-input wire:model="name" />
+        <x-text-input wire:model.live="name" />
         {{ $name }}
     </div>
 </div>
